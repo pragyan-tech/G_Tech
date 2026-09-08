a795d09efd95451f6715377b5798cfd6cef6f892
Pragyan <ozapragyan@gmail.com>
2026-09-09 00:00:48 +0530

    spec tables restructured per machine type and GST added to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -24,6 +24,7 @@ export default function Footer() {
               <span key={line}>{line}</span>
             ))}
           </address>
+          <p className="footer__gst">GST: 27ARAPJ5794R1ZX</p>
         </div>
 
         <div className="footer__col">
